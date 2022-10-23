b0871bce4e796d95f89420bc36eb061a3651559d
hetshah1011 <Shahhet1011@gmail.com>
2022-10-22 23:10:38 -0400

    added keyboard controller

--- WordleGame/ViewController.swift
+++ WordleGame/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var q_key: UIButton!
+    
+    @IBOutlet weak var w_key: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    @IBAction func keyboardPres(sender: UIButton) {
+        switch sender {
+        case q_key:
+            // Change Language to French
+            print ("q is pressed")
+            break
+        case w_key:
+            // or Spanish
+            print ("w is pressed")
+            break
+        default:
+            break
 
+        }
 
-}
+    }}
 
